2902a021ccf4ec10b9ffb58fff94773a2e0c82b2
Bansikumar Viradiya <bansiviradiya77@gmail.com>
2025-11-24 12:49:09 -0600

    created folder structure

---
+++ template.py
@@ -0,0 +1,43 @@
+#  Using this template we will create a folder structure for the project
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+
+project_name = "CNNClassifier"
+
+list_of_flies = [
+    ".github/workflows/.gitkeep",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utlis/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/contants/__init__.py",
+    "config/config.yaml",
+    "dvc.yaml",
+    "params.yaml",
+    "requirements.txt",
+    "setup.py",
+    "research/trials.ipynb"
+]
+
+
+for filepath in list_of_flies:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+
+    if filedir !="":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory: {filedir}")
+    
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        # we will create a file 
+        with open(filepath,"w") as f:
+            pass
+            logging.info(f"creating the empty file: {filepath}")
+    else:
+        logging.info(f"{filename} is already existing!!")
+    
